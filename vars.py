eaba2e298771681da5995dee67d903346fa1b0bd
kharazmi2 <155453982+kharazmi2@users.noreply.github.com>
2024-01-02 17:12:13 +0330

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,7 +2,7 @@ import tkinter
 from tkinter import *
 root = Tk()
 root.title("مدیریت دانش آموز")
-root.geometry("800x600")
+root.geometry("500x300")
 root.configure(bg="#ffd5ad")
 buttons = [Button(root, text="ورود", bg="#2e1600", fg="#ffd5ad"), Button(root, text="نمایش", bg="#2e1600", fg="#ffd5ad")]
 labs = [Label(root, text="مدیریت دانش آموز", font=("B Titr", 40), fg="#2e1600", bg="#ffd5ad"), Label(root, text="تهیه شده توسط داس", font=("B Titr", 25), bg="#ffd5ad", fg = "#2e1600")]
